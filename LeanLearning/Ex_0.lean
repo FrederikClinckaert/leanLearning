393e2d57c461f5c2d53f915abe9ce274278704f5
frederikclinckaert <r1046715@kuleuven.be>
2026-03-04 08:56:28 +0100

    SOme more exercises

--- LeanLearning/Ex_0.lean
+++ LeanLearning/Ex_0.lean
@@ -81,13 +81,55 @@ theorem Tg (a b c d : Nat)
   _ = d                   := h3
 
 -- OEFENING 8
-theorem T2 (a b c d : Nat)
+theorem Th (a b c d : Nat)
     (h1 : a = b + 4)
     (h2 : b = 2 * c)
     (h3 : d = 4 + 2 * c)
     : a = d :=
   calc
-    a = b + 4       := h1
-  _ = 2 * c + 4     := congrArg (fun x => x + 4) h2
-  _ = 4 + 2 * c     := Nat.add_comm (2 * c) 4
-  _ = d             := Eq.symm h3
+      a = b + 4       := h1
+    _ = 2 * c + 4     := congrArg (fun x => x + 4) h2
+    _ = 4 + 2 * c     := Nat.add_comm (2 * c) 4
+    _ = d             := Eq.symm h3
+
+-- OEFENING 9
+theorem Ti (a b c d e f : Nat)
+    (h1 : a = 3 * b)
+    (h2 : b = c + 2)
+    (h3 : c = 2 * d)
+    (h4 : 3 * (2 * d + 2) = e + 6)
+    (h5 : e + 6 = f)
+    : a = f :=
+  calc
+      a = 3 * b               := h1
+    _ = 3 * (c+2)             := congrArg (fun x => 3 * x) h2
+    _ = 3 * (2 * d + 2)       := congrArg (fun x => 3 * (x + 2)) h3
+    _ = e + 6                 := h4
+    _ = f                     := h5
+
+-- OEFENING 10
+theorem Tj (a b c d e : Nat)
+    (h1 : a = b + c)
+    (h2 : b = 2 * d)
+    (h3 : e = c + 2 * d)
+    : a = e :=
+  calc
+      a = b + c       := h1
+    _ = 2 * d + c     := congrArg (fun x => x + c) h2
+    _ = c + 2 * d     := Nat.add_comm (2*d) c
+    _ = e             := Eq.symm h3
+
+-- OEFENING 11
+theorem Tk (a b c d e f : Nat)
+    (h1 : a = 2 * b + 1)
+    (h2 : b = 3 * c)
+    (h3 : c = d + 1)
+    (h4 : 3 * (d + 1) = e)
+    (h5 : 2 * e + 1 = f)
+    : a = f :=
+  calc
+      a = 2 * b + 1            :=h1
+    _ = 2 * (3 * c) + 1        := congrArg (fun x=> 2 * x + 1) h2
+    _ = 2 * (3 * (d + 1)) + 1  := congrArg (fun x=> 2 * (3 * x) + 1) h3
+    _ = 2 * e + 1              := congrArg (fun x=> 2 * x + 1) h4
+    _ = f                      := h5
